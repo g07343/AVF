87b3be4386cadd48059da85a6a701283edaef235
Matthew Lewis <Loneeagle14@aol.com>
2013-08-08 13:49:32 -0500

    copied over to ios
    Copied all assets from last commit over to iOS "branch".

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -4,5 +4,16 @@
 
 $('#home').on('pageinit', function(){
 
+});
 
+$("#serviceChoose").on('change' ,function(){
+		
+		if ($("#serviceChoose").val() == "instagram") {
+			$("#instagramForm").css("display", "block");
+			$("#flickrForm").css("display", "none");
+		}
+		if ($("#serviceChoose").val() == "flickr") {
+			$("#instagramForm").css("display", "none");
+			$("#flickrForm").css("display", "block");
+		}
 });
